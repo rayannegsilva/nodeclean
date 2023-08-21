f58458867e6e346ecb093764e70bc61c2c1ff3a6
rayannegsilva <rgilodasilva@gmail.com>
2023-08-21 08:58:27 -0300

    chore: add jest configs for mongodb

--- jest.config.js
+++ jest.config.js
@@ -22,6 +22,7 @@ const config = {
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  preset: '@shelf/jest-mongodb',
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
