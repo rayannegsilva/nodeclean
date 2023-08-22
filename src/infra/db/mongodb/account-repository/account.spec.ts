e2adaf175f87e132116354697e00547f60c06517
rayannegsilva <rgilodasilva@gmail.com>
2023-08-22 10:16:59 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,30 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+describe('Account Mongo Repository', () => {
+  const MONGO_URL = process.env.MONGO_URL ?? 'any_url'
+
+  beforeAll(async () => {
+    await MongoHelper.connect(MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  it('should return an a account on a success', async () => {
+    const sut = new AccountMongoRepository()
+
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@email.com',
+      password: 'any_password'
+    })
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@email.com')
+    expect(account.password).toBe('any_password')
+  })
+})
