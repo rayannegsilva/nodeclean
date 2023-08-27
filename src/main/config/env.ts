3c68aa44beef3c497b51736ae0a2cb4509d38a23
rayannegsilva <rgilodasilva@gmail.com>
2023-08-27 19:29:57 -0300

    refactor: change env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export default {
-  MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-mongodb',
+  MONGO_URL: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/clean-mongodb',
   port: process.env.PORT || 3001
 }
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,11 +1,8 @@
 import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 import env from './config/env'
 
-console.log(env.MONGO_URL)
-
 MongoHelper.connect(env.MONGO_URL)
   .then(async () => {
-    console.log(env.MONGO_URL)
     const app = (await (import('./config/app'))).default
     app.listen(env.port, () => {
       console.log(`Server Running at http://localhost:${env.port}`)
